474c4753e1c92e461a7f66bab6271f35e775aec8
Ismaili1995 <ismailimohamedi1995@gmail.com>
2020-04-22 16:16:44 +0300

    orcadeco website codebase to repo

---
+++ mainoperation/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here
+class Category(models.Model):
+    name= models.CharField(max_length=50)
+    Create_on=models.DateField(auto_now=True)
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    def __str__(self):
+        """Unicode representation of Viewpoint."""
+        return self.name
+
+    
+
+class Product(models.Model):
+    name= models.CharField(max_length=50)
+    description= models.TextField(max_length=500)
+    image= models.FileField(max_length=100,null=True,blank=True)
+    Create_on=models.DateField(auto_now=True)
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    def __str__(self):
+        """Unicode representation of Viewpoint."""
+        return self.name
+
+
+class Promotion(models.Model):
+    name=models.CharField(max_length=50)
+    description=models.TextField(max_length=500)
+    image=models.CharField(max_length=50)
+    discount=models.CharField(max_length=100)
+    Create_on=models.DateField(auto_now=True)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    
+    
---
+++ mainoperation/views.py
@@ -0,0 +1,240 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, "mainoperation/index.html")
+
+
+def living(request):
+    return render(request, "Living/Living.html")
+
+
+def dining(request):
+    return render(request, "Dining/dining.html")
+
+
+def decoration(request):
+    return render(request, "Decoration/decoration.html")
+
+
+def office(request):
+    return render(request, "Office/office.html")
+
+
+def rugs(request):
+    return render(request, "Rugs/rugs.html")
+
+
+def beedroom(request):
+    return render(request, "BeedRoom/beedroom.html")
+
+
+def kitchen(request):
+    return render(request, "Kitchen/kitchen.html")
+
+
+def otheritem(request):
+    return render(request, "OtherItem/otheritem.html")
+
+
+def promotion(request):
+    return render(request, "Promotion/promotion.html")
+
+
+def coffetable(request):
+    return render(request, "Living/coffetable.html")
+
+
+def bars(request):
+    return render(request, "Living/bars.html")
+
+
+def chair(request):
+    return render(request, "Living/chair.html")
+
+
+def console(request):
+    return render(request, "Living/console.html")
+
+
+def livingroom(request):
+    return render(request, "Living/livingroom.html")
+
+
+def mirrors(request):
+    return render(request, "Living/mirrors.html")
+
+
+def sofabed(request):
+    return render(request, "Living/sofabed.html")
+
+
+def sofa(request):
+    return render(request, "Living/sofa.html")
+
+
+def tvmedia(request):
+    return render(request, "Living/tvmedia.html")
+
+
+def delivery(request):
+    return render(request, "Delivery/delivery.html")
+
+
+def buffet(request):
+    return render(request, "Dining/buffet.html")
+
+
+def diningchair(request):
+    return render(request, "Dining/diningchair.html")
+
+
+def diningseat(request):
+    return render(request, "Dining/diningseat.html")
+
+
+def desktable(request):
+    return render(request, "Office/desktable.html")
+
+
+def officechair(request):
+    return render(request, "Office/officechair.html")
+
+
+def officesofa(request):
+    return render(request, "Office/officesofa.html")
+
+
+def sofas(request):
+    return render(request, "Office/sofas.html")
+
+
+def officestationary(request):
+    return render(request, "Office/officestationary.html")
+
+
+def candle(request):
+    return render(request, "Decoration/candle.html")
+
+
+def homedecoration(request):
+    return render(request, "Decoration/homedecoration.html")
+
+
+def menuitem(request):
+    return render(request, "Decoration/menuitem.html")
+
+
+def homeaccessories(request):
+    return render(request, "Decoration/homeaccessories.html")
+
+
+def beedroomset(request):
+    return render(request, "BeedRoom/beedroomset.html")
+
+
+def childrenbeedroom(request):
+    return render(request, "BeedRoom/childrenbeedroom.html")
+
+
+def wardrobe(request):
+    return render(request, "BeedRoom/wardrobe.html")
+
+
+def kitchenaccessories(request):
+    return render(request, "Kitchen/kitchenaccessories.html")
+
+
+def kitchenfurniture(request):
+    return render(request, "Kitchen/kitchenfurniture.html")
+
+
+def kitchenteacup(request):
+    return render(request, "Kitchen/kitchenteacup.html")
+
+
+def kitchenline(request):
+    return render(request, "Kitchen/kitchenline.html")
+
+
+def kitchenelectronics(request):
+    return render(request, "Kitchen/kitchenelectronics.html")
+
+
+def kitchenplastics(request):
+    return render(request, "Kitchen/kitchenplastics.html")
+
+
+def lighting(request):
+    return render(request, "OtherItem/lighting.html")
+
+
+def sport(request):
+    return render(request, "OtherItem/sport.html")
+
+
+def luandrycleaning(request):
+    return render(request, "OtherItem/luandrycleaning.html")
+
+
+def children(request):
+    return render(request, "OtherItem/children.html")
+
+
+def bathroom(request):
+    return render(request, "OtherItem/bathroom.html")
+
+
+def bedding(request):
+    return render(request, "Rugs/bedding.html")
+
+
+def curtainrail(request):
+    return render(request, "Rugs/curtainrail.html")
+
+
+def curtainblinds(request):
+    return render(request, "Rugs/curtainblinds.html")
+
+
+def curtainpillows(request):
+    return render(request, "Rugs/curtainpillows.html")
+
+
+def doormats(request):
+    return render(request, "Rugs/doormats.html")
+
+
+def chairpads(request):
+    return render(request, "Rugs/chairpads.html")
+
+
+def outdoormattrees(request):
+    
+    return render(request, "Rugs/outdoormattreess.html")
+
+
+def plaids(request):
+    return render(request, "Rugs/plaids.html")
+
+
+def quilts(request):
+    return render(request, "Rugs/quilts.html")
+
+
+def rugstextiles(request):
+    return render(request, "Rugs/rugstextiles.html")
+
+
+def basketboxes(request):
+    return render(request, "Decoration/basketboxes.html")
+
+
+def teabacks(request):
+    return render(request, "Rugs/teabacks.html")
+def about(request):
+    return render(request, "Pages/about.html")
+def contact(request):
+    return render(request, "Pages/contact.html")
+def sample(request):
+    return render(request, "Rugs/sample.html")
